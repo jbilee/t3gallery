af937cde6a2aa9faefdd70db719f498f94b522d1
jbilee <julishahn@gmail.com>
2024-04-16 00:15:51 +0900

    scaffold basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://i.imgur.com/jo94Ux4.jpeg",
+  "https://i.imgur.com/sv7Yx3I.jpeg",
+  "https://i.imgur.com/I5wpnxA.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((img) => (
+          <div key={img.id} className="w-48">
+            <img src={img.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
